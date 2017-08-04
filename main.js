d56d0c744139db2f5d0ee5e00bb29db8cc34bc71
Jon Zachary <jontzachary@gmail.com>
2017-08-04 15:48:16 -0400

    add comments

--- main.js
+++ main.js
@@ -48,19 +48,31 @@ function compare(choice1, choice2) {
 function addWin() {
   if (choices[0] === choices[1]) return;
   const winner = compare(choices[0], choices[1]);
+  // I'm abusing the ternary operator!
   (winner === choices[0]) ? player1Wins += 1 : player2Wins += 1;
   $els.player1.textContent = player1Wins;
   $els.player2.textContent = player2Wins;
 }
 
 // create a function that will handle the click events. The function will check
-// to see what the user clicks, pushes it into an array
+// to see what the user clicks, push it into an array and turn the div that was clicked pink.
+// then it checks to see if there have been two selections made and if they have,it runs the
+// compare function we wrote earlier. Finally it changes the dom to reflect
+// the results of the compare function.
+// this is a messy function and too long, needs to be refactored.
 function checkChoiceAndCompare(e) {
+  // make sure that when we assign this to an event listener it only listens
+  // to what we want
   if (e.target.classList[0] !== 'rock' &&
       e.target.classList[0] !== 'paper' &&
       e.target.classList[0] !== 'scissors') return;
+  // push the class of the thing we clicked into the choices array
+  // and turn the thing light pink
   choices.push(e.target.classList[0]);
   e.target.style.backgroundColor = 'lightpink';
+  // check to see if two choices have been made, if they have
+  // use the compare function to find a winner and update the dom
+  // remove the event listener becasue the game is over
   if (choices.length === 2) {
     const choice1 = choices[0];
     const choice2 = choices[1];
@@ -71,7 +83,7 @@ function checkChoiceAndCompare(e) {
   }
 }
 
-
+// resets the dom and the choice array and adds the event listener
 function reset() {
   choices.splice(0, 2);
   $els.rock.style.backgroundColor = 'goldenrod';
@@ -80,6 +92,6 @@ function reset() {
   $els.winner.textContent = '';
   $els.container.addEventListener('click', checkChoiceAndCompare);
 }
-
+// adds one event listener to the div container 
 $els.button.addEventListener('click', reset);
 $els.container.addEventListener('click', checkChoiceAndCompare);
